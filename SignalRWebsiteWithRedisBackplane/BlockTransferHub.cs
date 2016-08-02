24f2bb537bcd0498bf97f4d5f9722d50b0608475
Holger Kenn <kenn-github@cubeos.org>
2016-08-02 07:53:22 +0200

    Added signalR service

---
+++ SignalRWebsiteWithRedisBackplane/BlockTransferHub.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+
+namespace SignalRWebsiteWithRedisBackplane
+{
+    public class BlockTransferHub : Hub
+    {
+        public void BlockSend(byte[] block)
+        {
+            Clients.All.broadcastBlock(block);
+
+        }
+        public void BlockGroupSend(string group,  byte[] block)
+        {
+            Clients.Group(group,Context.ConnectionId).multicastBlock(block); // sent to everybody in the group but me
+        }
+        public void ControlSend(string message)
+        {
+            Clients.All.broadcastControl(message);
+        }
+        public void ControlGroupSend(string group,string message)
+        {
+            Clients.Group(group, Context.ConnectionId).multicastControl(message); // sent to everybody in the group but me
+        }
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+        public void Join(string group)
+        {
+            Groups.Add(Context.ConnectionId, group);
+        }
+        public void Leave(string group)
+        {
+            Groups.Remove(Context.ConnectionId, group);
+        }
+
+
+    }
+}
---
+++ SignalRWebsiteWithRedisBackplane/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+using StackExchange.Redis;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Redis;
+using SignalRWebsiteWithRedisBackplane;
+
+namespace SignalRWebsiteWithRedisBackplane
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            var configuration = new RedisScaleoutConfiguration(/*<connection string goes here*/"", "AppName");
+            GlobalHost.DependencyResolver.UseRedis(configuration);
+            app.MapSignalR();
+        }
+    }
+}
+
